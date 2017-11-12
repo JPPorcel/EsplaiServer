99c12ad5182267ef65bcd366fca220b9ee1cdc97
JPPorcel <juanpiporcel@gmail.com>
2017-11-12 17:11:34 +0100

    default pass mysql

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express = require("express"),
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : 'toor',
+	password : 'root',
 	database : 'Esplai'
 });
 
